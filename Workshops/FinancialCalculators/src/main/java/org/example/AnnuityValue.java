6651b8828e5bb6fbce89dceb782c18b0e36608a0
AndrewC243 <andrew1tree@gmail.com>
2024-04-12 15:55:38 -0500

    Polishing code

--- Workshops/FinancialCalculators/src/main/java/org/example/AnnuityValue.java
+++ Workshops/FinancialCalculators/src/main/java/org/example/AnnuityValue.java
@@ -13,23 +13,17 @@ public class AnnuityValue {
 
     public double presentValue() {
         double r = expectedInterest / 100;
-        // P = PMT * (1 - (1 / (1 + r)^n)) / r
-//        Present Value (PV) of Annuity = (A ÷ r) (1 – (1 ÷ (1 + r) ^ t))
 //        C * (1 - (1 + i)^-n) / i
-
-//        return monthlyPayout
-//                * (1 - (1 / Math.pow(1 + (expectedInterest / 100), 12 * years))) / (expectedInterest / 100);
-//        return (monthlyPayout / r) * (1 - (1 / Math.pow(1 + r, years * 12)));
         return monthlyPayout * (1 - Math.pow(1 + r / 12, years * -12)) / (r / 12);
     }
 
     public double getMonthlyPayout() {
         return monthlyPayout;
     }
-    public void setMonthlyPayout(double monthlyPayout) {
-        this.monthlyPayout = monthlyPayout;
-    }
     public double getExpectedInterest() {
         return expectedInterest;
+    }
+    public int getYears() {
+        return years;
     }
 }
